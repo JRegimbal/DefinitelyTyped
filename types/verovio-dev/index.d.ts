422b2b34f01ef509b5c8537f7d21dbeebd3290d0
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-08-21 14:22:19 -0400

    Add EditorAction type and VerovioOptions types.

--- types/verovio-dev/index.d.ts
+++ types/verovio-dev/index.d.ts
@@ -4,10 +4,129 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.2
 
+declare type EditorAction = {
+  action: string;
+  param: object | EditorAction[];
+};
+
+declare type GeneralVerovioOptions = {
+  // General
+  adjustPageHeight: boolean;
+  breaks: string;
+  evenNoteSpacing: boolean;
+  humtype: boolean;
+  landscape: boolean;
+  mensuralToMeasure: boolean;
+  midiTempoAdjustment: number;
+  minLastJusticiation: number;
+  mmOutput: boolean;
+  noFooter: boolean;
+  noHeader: boolean;
+  noJustification: boolean;
+  openControlEvents: boolean;
+  pageHeight: number;
+  pageMarginBottom: number;
+  pageMarginLeft: number;
+  pageMarginRight: number;
+  pageMarginTop: number;
+  pageWidth: number;
+  svgBoundingBoxes: boolean;
+  svgViewBox: boolean;
+  unit: number;
+  useFacsimile: boolean;
+  usePgFooterForAll: boolean;
+  usePgHeaderForAll: boolean;
+};
+
+declare type GeneralLayoutVerovioOptions = {
+  // General Layout
+  barLineWidth: number;
+  beamMaxSlope: number;
+  beamMinSlope: number;
+  font: string;
+  graceFactor: number;
+  graceRhythmAlign: boolean;
+  graceRightAlign: boolean;
+  hairpinSize: number;
+  leftPosition: number;
+  lyricHyphenLength: number;
+  lyricHyphenWidth: number;
+  lyricNoStartHyphen: boolean;
+  lyricSize: number;
+  lyricTopMinMargin: number;
+  measureMinWidth: number;
+  measureNumber: string;
+  slurControlPoints: number;
+  slurCurveFactor: number;
+  slurHeightFactor: number;
+  slurMaxHeight: number;
+  slurMaxSlope: number;
+  slurMinHeight: number;
+  slurThickness: number;
+  spacingDurDetection: boolean;
+  spacingLinear: number;
+  spacingNonLinear: number;
+  spacingStaff: number;
+  spacingSystem: number;
+  staffLineWidth: number;
+  stemWidth: number;
+  tieThickness: number;
+};
+
+declare type SelectorsVerovioOptions = {
+  appXPathQuery: string[];
+  choiceXPathQuery: string[];
+  mdivXPathQuery: string;
+  substXPathQuery: string[];
+};
+
+declare type ElementMarginsVerovioOptions = {
+  defaultBottomMargin: number;
+  defaultLeftMargin: number;
+  defaultRightMargin: number;
+  defaultTopMargin: number;
+  bottomMarginHarm: number;
+  leftMarginAccid: number;
+  leftMarginBarLine: number;
+  leftMarginBeatRpt: number;
+  leftMarginChord: number;
+  leftMarginClef:  number;
+  leftMarginKeySig: number;
+  leftMarginLeftBarLine: number;
+  leftMarginMensur: number;
+  leftMarginMeterSig: number;
+  leftMarginMRest: number;
+  leftMarginMRpt2: number;
+  leftMarginMultiRest: number;
+  leftMarginMultiRpt: number;
+  leftMarginNote: number;
+  leftMarginRest: number;
+  leftMarginRightBarLine: number;
+
+  rightMarginAccid: number;
+  rightMarginBarLine: number;
+  rightMarginBeatRpt: number;
+  rightMarginChord: number;
+  rightMarginClef: number;
+  rightMarginKeySig: number;
+  rightMarginLeftBarLine: number;
+  rightMarginMensur: number;
+  rightMarginMeterSig: number;
+  rightMarginMRest: number;
+  rightMarginMRpt2: number;
+  rightMarginMultiRest: number;
+  rightMarginNote: number;
+  rightMarginRest: number;
+  rightMarginRightBarLine: number;
+  topMarginHarm: number;
+};
+
+declare type VerovioOptions = GeneralVerovioOptions & GeneralLayoutVerovioOptions & SelectorsVerovioOptions & ElementMarginsVerovioOptions;
+
 declare class toolkit {
   constructor();
   destroy(): void;
-  edit(editorAction: object): boolean;
+  edit(editorAction: EditorAction): boolean;
   editInfo(): object;
   getAvailableOptions(): object;
   getElementAttr(xmlId: string): object;
@@ -16,20 +135,20 @@ declare class toolkit {
   getLog(): string;
   getMEI(page_no: number, score_based: boolean): string;
   getMIDIValuesForElement(xmlId: string): object;
-  getOptions(default_values: boolean): object;
+  getOptions(default_values: boolean): VerovioOptions;
   getPageCount(): number;
   getPageWithElement(xmlId: string): number;
   getTimeForElement(xmlId: string): number;
   getVersion(): string;
   loadData(data: string): boolean;
-  renderToMIDI(options: object): string;
-  renderToSVG(page_no: number, options: object): string;
+  renderToMIDI(options: Partial<VerovioOptions>): string;
+  renderToSVG(page_no: number, options: Partial<VerovioOptions>): string;
   renderToTimemap(): object;
   redoLayout(): void;
   redoPagePitchPosLayout(): void;
-  renderData(data: string, options: object): string;
-  renderPage(pageNo: number, options: object): string;
-  setOptions(options: object): void;
+  renderData(data: string, options: Partial<VerovioOptions>): string;
+  renderPage(pageNo: number, options: Partial<VerovioOptions>): string;
+  setOptions(options: Partial<VerovioOptions>): void;
 }
 
 declare const verovio: {
@@ -40,7 +159,7 @@ declare const verovio: {
   vrvToolkit: {
     constructor: () => number;
     destructor(ptr: number): void;
-    edit(ptr: number, editorAction: object): boolean;
+    edit(ptr: number, editorAction: string): boolean;
     getAvailableOptions(ptr: number): string;
     getElementAttr(ptr: number, xmlId: string): string;
     getElementsAtTime(ptr: number, millisec: number): string;
