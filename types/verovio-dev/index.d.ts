de1248dd2a477bf810bfe53c2b52e9a113c81b06
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-08-21 12:41:52 -0400

    Correct parameters that are parsed as JSON objects.

--- types/verovio-dev/index.d.ts
+++ types/verovio-dev/index.d.ts
@@ -6,38 +6,40 @@
 declare class toolkit {
   constructor();
   destroy(): void;
-  edit(editorAction: string): boolean;
-  getAvailableOptions(): string;
-  getElementAttr(xmlId: string): string;
-  getElementsAtTime(millisec: number): string;
+  edit(editorAction: object): boolean;
+  editInfo(): object;
+  getAvailableOptions(): object;
+  getElementAttr(xmlId: string): object;
+  getElementsAtTime(millisec: number): object;
   getHumdrum(): string;
   getLog(): string;
   getMEI(page_no: number, score_based: boolean): string;
-  getMIDIValuesForElement(xmlId: string): string;
-  getOptions(default_values: boolean): string;
+  getMIDIValuesForElement(xmlId: string): object;
+  getOptions(default_values: boolean): object;
   getPageCount(): number;
   getPageWithElement(xmlId: string): number;
   getTimeForElement(xmlId: string): number;
   getVersion(): string;
   loadData(data: string): boolean;
-  renderToMIDI(c_options: string): string;
-  renderToSVG(page_no: number, c_options: string): string;
-  renderToTimemap(): string;
+  renderToMIDI(options: object): string;
+  renderToSVG(page_no: number, options: object): string;
+  renderToTimemap(): object;
   redoLayout(): void;
   redoPagePitchPosLayout(): void;
-  renderData(data: string, options: string): string;
-  setOptions(options: string): void;
+  renderData(data: string, options: object): string;
+  renderPage(pageNo: number, options: object): string;
+  setOptions(options: object): void;
 }
 
 declare const verovio: {
   instances: number[];
   toolkit: {
-    new(): toolkit;
+    new (): toolkit;
   };
   vrvToolkit: {
     constructor(): number;
     destructor(ptr: number): void;
-    edit(ptr: number, editorAction: string): boolean;
+    edit(ptr: number, editorAction: object): boolean;
     getAvailableOptions(ptr: number): string;
     getElementAttr(ptr: number, xmlId: string): string;
     getElementsAtTime(ptr: number, millisec: number): string;
@@ -58,7 +60,7 @@ declare const verovio: {
     redoPagePitchPosLayout(ptr: number): void;
     renderData(ptr: number, data: string, options: string): string;
     setOptions(ptr: number, options: string): void;
-  }
-}
+  };
+};
 
 export default verovio;
